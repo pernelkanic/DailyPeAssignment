89875fa92ef0ee0c4d5bb6b0ba0aef132c427dd6
pernelkanic <whitehattt03@gmail.com>
2024-05-28 19:13:38 +0530

    initialised the folder structure

---
+++ src/test/java/com/example/DailyPe/dailype/DailypeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DailyPe.dailype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
