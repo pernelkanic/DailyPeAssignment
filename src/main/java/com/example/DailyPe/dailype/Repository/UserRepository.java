979defa5595560636d547751f6554efd40e9da30
pernelkanic <211001118@rajalakshmi.edu.in>
2024-05-28 20:26:59 +0530

    get functionality is implemented

--- src/main/java/com/example/DailyPe/dailype/Repository/UserRepository.java
+++ src/main/java/com/example/DailyPe/dailype/Repository/UserRepository.java
@@ -1,5 +1,12 @@
 package com.example.DailyPe.dailype.Repository;
 
-public interface UserRepository {
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.DailyPe.dailype.Models.Users;
+@Repository	
+public interface UserRepository extends	 JpaRepository<Users , UUID>{
 
 }
